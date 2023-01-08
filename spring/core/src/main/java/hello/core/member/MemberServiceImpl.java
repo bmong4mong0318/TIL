f47ce168ffb9ad147c4cacd4dc81945331be186f
YunDaeHyeok <bmong4mong0318@gmail.com>
2023-01-09 08:30:56 +0900

    test: Add MemberService test

--- spring/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ spring/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // MemberServiceImpl 은 MemberRepository(추상화)에도 의존하고 MemoryMemberRepository(구체화)에도 의존한다.
+    // -> 구체화 부분을 변경하려 할 때 문제가 된다. (DIP 위반)
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
