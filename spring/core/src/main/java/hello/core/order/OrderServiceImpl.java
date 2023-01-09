4eadcf8195a30d8dfc8066407ba42d7d1b8f09f3
YunDaeHyeok <bmong4mong0318@gmail.com>
2023-01-10 08:13:13 +0900

    chore: Add comments

--- spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,18 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    //`OrderServiceImpl`이 `DiscountPolicy` 인터페이스 뿐만 아니라 `FixDiscountPolicy` 구체 클래스도 함께 의존하고 있다.
+    // -> "DIP 위반"
+
+    //그래서 할인 정책을 변경하려면 클라이언트인 `OrderServiceImpl`코드를 고쳐야 한다(변경에 닫혀있지 않다).
+    //기능을 확장해서 변경하면, 클라이언트 코드에 영향을 준다 -> "OCP 위반"
+
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // 해결책 : 누군가가 클라이언트인 `OrderServiceImpl`에 `DiscountPolicy`의 구현 객체를 대신 생성해서 주입해주어야 한다.
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
