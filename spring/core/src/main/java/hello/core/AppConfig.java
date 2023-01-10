d38393d932950893291169664205df243f3b2490
YunDaeHyeok <bmong4mong0318@gmail.com>
2023-01-11 08:22:32 +0900

    refactor: Extract method

--- spring/core/src/main/java/hello/core/AppConfig.java
+++ spring/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -14,10 +16,20 @@ public class AppConfig {
         // 생성한 객체의 인스턴스의 참조(레퍼런스)를 '생성자를 통해서 주입(연결)' 해준다.
         // `MemberServiceImpl` 입장에서 보면 의존관계를 마치 외부에서 주입해주는 것 같다고 해서
         // DI(Dependency Injection) 의존성 주입이라고 한다.
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    // 역할이 드러나도록 함수를 분리한다.
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
