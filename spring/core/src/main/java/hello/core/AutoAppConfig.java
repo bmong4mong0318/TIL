451fd278d3f524f1b2b6bdaafb765efa3226e99b
YunDaeHyeok <bmong4mong0318@gmail.com>
2023-01-18 08:11:15 +0900

    feat: Add properties

--- spring/core/src/main/java/hello/core/AutoAppConfig.java
+++ spring/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -17,5 +20,16 @@ import org.springframework.context.annotation.FilterType;
     excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    //이 경우 수동빈 등록이 우선권을 가진다.
+    //수동 빈이 자동 빈을 오버라이딩 해버린다.
+    //개발자가 우선을 가지기 때문에 수동이 우선권을 가지는 것이 좋지만, 현실에서는 개발자의 의도보다는 여러 설정들이 꼬이는 경우가 많다.
+    //최근 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꾸었다.
+    //application.properties에 `spring.main.allow-bean-definition-overriding=true`를 등록하면 오류가 나지 않는다.
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 
 }
